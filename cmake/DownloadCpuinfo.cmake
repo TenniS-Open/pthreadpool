e5089b10fca6e4716b91f76f294a938ded14c2e5
sen.lv <sen.lv@seetatech.com>
2022-02-09 15:56:19 +0800

    redirect GIT_REPOSITORY to TenniS groups

--- cmake/DownloadCpuinfo.cmake
+++ cmake/DownloadCpuinfo.cmake
@@ -4,7 +4,7 @@ PROJECT(cpuinfo-download NONE)
 
 INCLUDE(ExternalProject)
 ExternalProject_Add(cpuinfo
-	GIT_REPOSITORY https://lvsen@gitlab.seetatech.com/lvsen/cpuinfo.git
+	GIT_REPOSITORY https://gitlab.seetatech.com/TenniS/cpuinfo
 	GIT_TAG 728f3e909fa5c57e0123c4658e234f6b1941385d
 	SOURCE_DIR "${CMAKE_BINARY_DIR}/cpuinfo-source"
 	BINARY_DIR "${CMAKE_BINARY_DIR}/cpuinfo"
--- cmake/DownloadFXdiv.cmake
+++ cmake/DownloadFXdiv.cmake
@@ -4,7 +4,7 @@ PROJECT(fxdiv-download NONE)
 
 INCLUDE(ExternalProject)
 ExternalProject_Add(fxdiv
-	GIT_REPOSITORY https://lvsen@gitlab.seetatech.com/lvsen/FXdiv.git
+	GIT_REPOSITORY https://gitlab.seetatech.com/TenniS/FXdiv
 	GIT_TAG master
 	SOURCE_DIR "${CMAKE_BINARY_DIR}/FXdiv-source"
 	BINARY_DIR "${CMAKE_BINARY_DIR}/FXdiv"
